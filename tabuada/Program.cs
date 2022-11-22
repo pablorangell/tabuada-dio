57ea0649cc233cfe59795265c406f8cd8c8abed0
Pablo Rangel <prangelldestroy@gmail.com>
2022-11-22 10:16:44 -0300

    tabuada

---
+++ tabuada/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tabuada
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int contador = 0; contador <= 10; contador++)
+            {
+                Console.WriteLine($"{numero} X {contador} = {numero * contador}");
+            }
+        }
+    }
+}
